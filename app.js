97fa34763bbb60591ddf1dd087dedff55c94051f
nahuelminio <nahuelbenjamin04@gmail.com>
2022-09-07 06:51:32 -0300

    subo proyecto terminado

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const { read } = require('fs')
+const path = require('path')
+
+const app=express()
+
+app.use(express.static(path.join(__dirname,'/public')))
+
+
+app.get('/',(req,res) => {
+    res.sendFile(path.resolve(__dirname,'./views/home.html'))
+})
+app.get('/login',(req,res) => {
+    res.sendFile(path.resolve(__dirname,'./views/login.html'))
+})
+app.get('/registro' , (req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/registro.html'))
+})
+app.get('/producto' , (req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/producto.html'))
+})
+app.listen(3009, () => { 
+    console.log('servidor corriendo en el puerto 3009')
+})
+
